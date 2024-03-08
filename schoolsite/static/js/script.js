f9bf96c77ee28653e6b3f17c38d64b986c04238d
Abdul rahmon <abdulrahmon2384@gmail.com>
2024-03-08 14:48:36 +0000

    index html seperated

---
+++ schoolsite/static/js/script.js
@@ -0,0 +1,177 @@
+// start: Sidebar
+const sidebarToggle = document.querySelector('.sidebar-toggle')
+const sidebarOverlay = document.querySelector('.sidebar-overlay')
+const sidebarMenu = document.querySelector('.sidebar-menu')
+const main = document.querySelector('.main')
+if(window.innerWidth < 768) {
+    main.classList.toggle('active')
+    sidebarOverlay.classList.toggle('hidden')
+    sidebarMenu.classList.toggle('-translate-x-full')
+}
+sidebarToggle.addEventListener('click', function (e) {
+    e.preventDefault()
+    main.classList.toggle('active')
+    sidebarOverlay.classList.toggle('hidden')
+    sidebarMenu.classList.toggle('-translate-x-full')
+})
+sidebarOverlay.addEventListener('click', function (e) {
+    e.preventDefault()
+    main.classList.add('active')
+    sidebarOverlay.classList.add('hidden')
+    sidebarMenu.classList.add('-translate-x-full')
+})
+document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        e.preventDefault()
+        const parent = item.closest('.group')
+        if (parent.classList.contains('selected')) {
+            parent.classList.remove('selected')
+        } else {
+            document.querySelectorAll('.sidebar-dropdown-toggle').forEach(function (i) {
+                i.closest('.group').classList.remove('selected')
+            })
+            parent.classList.add('selected')
+        }
+    })
+})
+// end: Sidebar
+
+
+
+// start: Popper
+const popperInstance = {}
+document.querySelectorAll('.dropdown').forEach(function (item, index) {
+    const popperId = 'popper-' + index
+    const toggle = item.querySelector('.dropdown-toggle')
+    const menu = item.querySelector('.dropdown-menu')
+    menu.dataset.popperId = popperId
+    popperInstance[popperId] = Popper.createPopper(toggle, menu, {
+        modifiers: [
+            {
+                name: 'offset',
+                options: {
+                    offset: [0, 8],
+                },
+            },
+            {
+                name: 'preventOverflow',
+                options: {
+                    padding: 24,
+                },
+            },
+        ],
+        placement: 'bottom-end'
+    });
+})
+document.addEventListener('click', function (e) {
+    const toggle = e.target.closest('.dropdown-toggle')
+    const menu = e.target.closest('.dropdown-menu')
+    if (toggle) {
+        const menuEl = toggle.closest('.dropdown').querySelector('.dropdown-menu')
+        const popperId = menuEl.dataset.popperId
+        if (menuEl.classList.contains('hidden')) {
+            hideDropdown()
+            menuEl.classList.remove('hidden')
+            showPopper(popperId)
+        } else {
+            menuEl.classList.add('hidden')
+            hidePopper(popperId)
+        }
+    } else if (!menu) {
+        hideDropdown()
+    }
+})
+
+function hideDropdown() {
+    document.querySelectorAll('.dropdown-menu').forEach(function (item) {
+        item.classList.add('hidden')
+    })
+}
+function showPopper(popperId) {
+    popperInstance[popperId].setOptions(function (options) {
+        return {
+            ...options,
+            modifiers: [
+                ...options.modifiers,
+                { name: 'eventListeners', enabled: true },
+            ],
+        }
+    });
+    popperInstance[popperId].update();
+}
+function hidePopper(popperId) {
+    popperInstance[popperId].setOptions(function (options) {
+        return {
+            ...options,
+            modifiers: [
+                ...options.modifiers,
+                { name: 'eventListeners', enabled: false },
+            ],
+        }
+    });
+}
+// end: Popper
+
+
+
+// start: Tab
+document.querySelectorAll('[data-tab]').forEach(function (item) {
+    item.addEventListener('click', function (e) {
+        e.preventDefault()
+        const tab = item.dataset.tab
+        const page = item.dataset.tabPage
+        const target = document.querySelector('[data-tab-for="' + tab + '"][data-page="' + page + '"]')
+        document.querySelectorAll('[data-tab="' + tab + '"]').forEach(function (i) {
+            i.classList.remove('active')
+        })
+        document.querySelectorAll('[data-tab-for="' + tab + '"]').forEach(function (i) {
+            i.classList.add('hidden')
+        })
+        item.classList.add('active')
+        target.classList.remove('hidden')
+    })
+})
+// end: Tab
+
+
+
+
+
+
+// Sorting Child's Performance table
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("performance-table");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.getElementsByTagName("tr");
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[sortIndex];
+            y = rows[i + 1].getElementsByTagName("td")[sortIndex];
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+// Get the index of the column to sort by
+var sortIndex = document.getElementById("sort-select").value;
+
+// Update sortIndex when sort option changes
+document.getElementById("sort-select").addEventListener("change", function() {
+    sortIndex = this.value;
+    sortTable();
+});
+// Sorting Child's Performance End
+
+
+
+
